2c4c1000e0811b8491281f0a7d8e32be107e1bac
MarcosNobre33 <m.luiz9431@yahoo.com.br>
2024-09-07 19:56:30 -0300

    Add files via upload

---
+++ atividade laboratorio2/dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rebaca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que concorre a medalha de ouro..",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira..",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca..",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+
+]
